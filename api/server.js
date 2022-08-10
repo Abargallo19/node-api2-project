478b5e2585d6c3830a14e7afcf9ab898b07c45bd
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-09 23:07:19 -0400

    initial commit

--- api/server.js
+++ api/server.js
@@ -1,2 +1,31 @@
 // implement your server here
 // require your posts router and connect it here
+const express = require('express');
+
+const server = express();
+
+const postMod = require('./posts/posts-model');
+
+const postRouter = require('./posts/posts-router');
+
+server.get('/api/posts', (req, res) => {
+    postMod.find()
+        .then(post => {
+            res.status(200).json(post);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({messsage: 'An error happenned'});
+        });
+});
+
+server.get('api/posts/:id', (req, res) => {
+    postMod.findById(req.params.id)
+    .then()
+    .catch()
+})
+
+
+
+
+module.exports = server;
