f74c6e70813315626a734654c125f202677a4137
Allesandria <mufasabugatti@Mufasas-MBP.lan>
2022-08-11 19:01:35 -0400

    put returns proper updated user info

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,27 +16,44 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    
+
     postMod.findById(req.params.id)
-    .then(result => {
-        if(!result){
-            res.status(404).json({message: "The post with the specified ID does not exist"})
-        } else {
-            res.status(200).json(result)
-        }
-    })
-    .catch(() => {
-        res.status(500).json({message: "The post information could not be retrieved"})
-    })
-})
+        .then(result => {
+            if (!result) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            } else {
+                res.status(200).json(result)
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The post information could not be retrieved" })
+        })
+});
+
+router.get('/:id/comments', (req, res) => {
+    postMod.findPostComments(req.params.id)
+        .then(comment => {
+            if (!comment) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+
+            } else {
+                postMod.findCommentById(comment)
+                res.status(200).json(comment)
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ message: "The comments information could not be retrieved" })
+        })
+});
+
 
 router.post('/', (req, res) => {
     const { title, contents } = req.body
-        if (!title || !contents) {
-            res.status(400).json({message: "Please provide title and contents for the post"});
-        } else{
-            postMod.insert({title, contents})
-            .then(({id}) => {
+    if (!title || !contents) {
+        res.status(400).json({ message: "Please provide title and contents for the post" });
+    } else {
+        postMod.insert({ title, contents })
+            .then(({ id }) => {
                 return postMod.findById(id)
             })
             .then(newPost => {
@@ -48,25 +65,74 @@ router.post('/', (req, res) => {
                     err: err
                 })
             })
-         
-        }
-    
+
+    }
+
 })
 
 router.delete('/:id', (req, res) => {
     const killSwitch = req.params.id
+    let post
     postMod.remove(killSwitch)
-    .then(index => {
-        if(index > 0){
-            res.status(200).json(index,  {message: "kill switch activated"})
-        } else {
-            res.status(404).json({message: "The post with the specified ID does not exist"})
-        }
-    })
-    .catch(() => {
-        res.status(500).json({message: "The post could not be removed"})
-    })
+        .then((postToDelete) => {
+
+            if (!postToDelete) {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+
+            } else {
+                post = postToDelete
+                // postMod.remove(killSwitch)
+                res.json(post)
+
+            }
+        })
+        // .then(() => {
+        //     res.status(200).json(post)
+        // })
+        .catch(() => {
+            res.status(500).json({ message: "The post could not be removed" })
+        })
+
 });
+
+router.put('/:id', async (req, res) => {
+    const { title, contents } = req.body;
+
+    if (!title || !contents) return res.status(400).json({ message: "Please provide title and contents for the post" })
+    try {
+        const updatedUserResult = await postMod.update(req.params.id, { title, contents })
+        if (!updatedUserResult) return res.status(404).json({ message: "The post with the specified ID does not exist" })
+
+        const { id, title: t, contents: c } = await postMod.findById(req.params.id)
+
+        res.status(200).json({ id, title: t, contents: c })
+    } catch (error) {
+        res.status(500).json({ message: "The post information could not be modified" })
+    }
+
+
+
+
+
+    //     postMod.update(req.params.id, { title, contents })
+    //     .then(
+    //         result => {
+    //             if(!result){
+    //                 res.status(404).json({message: "The post with the specified ID does not exist"})
+    //             } else {
+    //                 // res.status(200).json({id: +req.params.id, title, contents })
+
+    //             }
+    //             // return result;
+    //         }
+
+    //     )
+
+    // .catch(() => {
+    //     res.status(500).json({message: "The post information could not be modified"})
+    // })
+
+})
 
 
 module.exports = router;
